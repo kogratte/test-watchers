134107b3f3bf8e562309a0bb8074b5418bb763f7
Nicolas ISSALENE <nicolas.issalene@loftorbital.com>
2022-03-13 17:35:53 +0100

    Replace default test

--- tests/unit/components/HelloWorld.spec.ts
+++ tests/unit/components/HelloWorld.spec.ts
@@ -2,11 +2,9 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+  test('should display stars count, with a default value 0', () => {
+    const wrapper = shallowMount(HelloWorld)
+
+    expect(wrapper.get('[data-testid="stars-count"]').text()).toBe('0 stars')
   })
 })
